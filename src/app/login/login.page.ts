262a996633d87ad3326e1a16a0bc0296aea3938c
jonz94 <jody16888@gmail.com>
2020-07-31 14:22:22 +0800

    ✨ feat(ui): present alert message when login failed

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +10,26 @@ export class LoginPage {
   email = '';
   password = '';
 
-  constructor(private navController: NavController) {}
+  constructor(
+    private navController: NavController,
+    private alertController: AlertController,
+  ) {}
 
   submit() {
     if (this.email === 'jonz94@jonz94.dev' && this.password === '12345678') {
       this.navController.navigateForward('/tabs');
+    } else {
+      this.presentLoginFailedAlert();
     }
+  }
+
+  async presentLoginFailedAlert() {
+    const alert = await this.alertController.create({
+      header: 'Wrong email or password',
+      message: 'please try again',
+      buttons: ['OK'],
+    });
+
+    alert.present();
   }
 }
