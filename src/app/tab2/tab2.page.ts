278bec2ecdab12c0e5ca315af7ba8844d77767cd
yww1327 <yww1327@gmail.com>
2020-08-13 11:33:22 +0800

    0813

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Plugins, CameraResultType } from '@capacitor/core';
+
+const { Camera } = Plugins;
 
 @Component({
   selector: 'app-tab2',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  imageUrl = '';
+
   constructor() {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    console.log(image);
+    this.imageUrl = image.webPath;
+    // Can be set to the src of an image now
+    //imageElement.src = imageUrl;
+  }
 }
